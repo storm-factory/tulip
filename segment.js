bf03f3835d0b90d92afe1dfa7234cbb7cb34dfea
Drew Mitchell <drew.mitchell@wowza.com>
2015-10-09 08:02:09 -0600

    messing with parametric interpolation driven by the onchange event, very likely that we need to loop through the path and smooth for all points instead of having each handle modify knots along the entire path.

--- segment.js
+++ segment.js
@@ -1,27 +1,36 @@
 /*
+
+  Phase1
+  DONE: change quadradic curve to bezier curve
+  DONE: Re add listeners
+  TODO: interpolate curves on path
+
+  Phase2
   DONE: make the start point optional
-  TODO extend segment for off-piste, track, road, dual-carrage-way part types. Change number of parts and link points to move parts in parallel.
-  TODO figure out rotational geometry
-  DONE: decide if an how to link midpoints to work opposite of each other
   TODO show or hide points if this segment is clicked on
-  TODO edit length
+  TODO extend segment for off-piste, track, road, dual-carrage-way part types. Change number of parts and link points to move parts in parallel.
+
 */
 
 var Segment = Class({
 
   create: function(originX, originY, endX, endY, editOrigin) {
     fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
-    this.line = new fabric.Path('M 150 300 Q 150, 252.5, 150, 225 M 150 225 Q 150, 197.5, 150, 150', { fill: '', stroke: 'black', strokeWidth: 3, hasControls: true, selectable: false });
-
+    // this.line = new fabric.Path('M 150 300 C 150, 275, 150, 250 M 150 250 C 150, 225, 150, 200 M 150 200 C 150, 175, 150, 150', { fill: '', stroke: 'black', strokeWidth: 3, hasControls: true, selectable: false });
+    this.line = new fabric.Path('M 150 300 C 150, 285, 150, 265, 150, 250 C 150, 235, 150, 215, 150, 200 C 150, 185, 150, 165, 150, 150', { fill: '', stroke: 'black', strokeWidth: 3, hasControls: true, selectable: false });
     canvas.add(this.line);
 
     this.origin = this.makeOrigin(editOrigin);
 
-    this.join = this.makeCurveEndPoint(this.line.path[1][3], this.line.path[1][4]);
-    this.join.name = "join";
-    canvas.add(this.join);
+    this.joinOne = this.makeCurveEndPoint(this.line.path[1][5], this.line.path[1][6]);
+    this.joinOne.name = "joinOne";
+    canvas.add(this.joinOne);
 
-    this.end = this.makeCurveEndPoint(this.line.path[3][3], this.line.path[3][4]);
+    this.joinTwo = this.makeCurveEndPoint(this.line.path[2][5], this.line.path[2][6]);
+    this.joinTwo.name = "joinTwo";
+    canvas.add(this.joinTwo);
+    //
+    this.end = this.makeCurveEndPoint(this.line.path[3][5], this.line.path[3][6]);
     this.end.name = "end";
     canvas.add(this.end);
 
@@ -55,22 +64,6 @@ var Segment = Class({
     return c;
   },
 
-  makeCurveMidPoint: function(left, top, line1, line2, line3){
-    var c = new fabric.Circle({
-      left: left,
-      top: top,
-      strokeWidth: 1,
-      radius: 4,
-      fill: 'yellow',
-      stroke: '#666',
-      bound_to: line1
-    });
-
-    c.hasBorders = c.hasControls = false;
-    c.line = this.line;
-    return c;
-  },
-
   pointMoving: function(e){
     var p = e.target;
 
@@ -91,43 +84,80 @@ var Segment = Class({
       p.line.path[0][1] = p.left;
       p.line.path[0][2] = p.top;
 
-      var controlPoints = getControlPoints(p.line.path[0][1],p.line.path[0][2],p.line.path[1][3],p.line.path[1][4],p.line.path[3][3],p.line.path[3][4],.7);
-
+      var controlPoints = getControlPoints(p.line.path[0][1],p.line.path[0][2],p.line.path[0][1],p.line.path[0][2],p.line.path[1][3],p.line.path[1][4],.4);
+      //
       p.line.path[1][1] = controlPoints[0];
       p.line.path[1][2] = controlPoints[1];
-
-      p.line.path[3][1] = controlPoints[2];
-      p.line.path[3][2] = controlPoints[3];
-
 
     }else if(p.name == "end"){
-      p.line.path[3][3] = p.left;
-      p.line.path[3][4] = p.top;
+      p.line.path[3][5] = p.left;
+      p.line.path[3][6] = p.top;
 
-      var controlPoints = getControlPoints(p.line.path[0][1],p.line.path[0][2],p.line.path[1][3],p.line.path[1][4],p.line.path[3][3],p.line.path[3][4],.7);
+      var controlPoints = getControlPoints(p.line.path[1][5],p.line.path[1][6],p.line.path[3][5],p.line.path[3][6],p.line.path[3][5],p.line.path[3][6],.4);
 
-      p.line.path[1][1] = controlPoints[0];
-      p.line.path[1][2] = controlPoints[1];
+      p.line.path[3][1] = controlPoints[0];
+      p.line.path[3][2] = controlPoints[1];
+
+      p.line.path[3][3] = controlPoints[2];
+      p.line.path[3][4] = controlPoints[3];
+
+      controlPoints = getControlPoints(p.line.path[1][5],p.line.path[1][6],p.line.path[2][5],p.line.path[2][6],p.line.path[3][5],p.line.path[3][6],.4);
+
+      p.line.path[2][3] = controlPoints[0];
+      p.line.path[2][4] = controlPoints[1];
 
       p.line.path[3][1] = controlPoints[2];
       p.line.path[3][2] = controlPoints[3];
 
 
-    } else if(p.name == "join") {
+    } else if(p.name == "joinOne") {
+      //Move the ends of the paths
+      p.line.path[1][5] = p.left;
+      p.line.path[1][6] = p.top;
+
+      var controlPoints = getControlPoints(p.line.path[0][1],p.line.path[0][2],p.line.path[1][5],p.line.path[1][6],p.line.path[2][5],p.line.path[2][6],.4);
+
+      p.line.path[1][1] = controlPoints[0];
+      p.line.path[1][2] = controlPoints[1];
+
+      p.line.path[1][3] = controlPoints[0];
+      p.line.path[1][4] = controlPoints[1];
+
+      p.line.path[2][1] = controlPoints[2];
+      p.line.path[2][2] = controlPoints[3];
+
+      controlPoints = getControlPoints(p.line.path[1][5],p.line.path[1][6],p.line.path[2][5],p.line.path[2][6],p.line.path[3][5],p.line.path[3][6],.4);
+
+      p.line.path[2][3] = controlPoints[0];
+      p.line.path[2][4] = controlPoints[1];
+
+      p.line.path[3][1] = controlPoints[2];
+      p.line.path[3][2] = controlPoints[3];
+    } else if(p.name == "joinTwo") {
 
       //Move the ends of the paths
-      p.line.path[1][3] = p.left;
-      p.line.path[1][4] = p.top;
-      p.line.path[2][1] = p.left;
-      p.line.path[2][2] = p.top;
+      p.line.path[2][5] = p.left;
+      p.line.path[2][6] = p.top;
 
-      var controlPoints = getControlPoints(p.line.path[0][1],p.line.path[0][2],p.line.path[1][3],p.line.path[1][4],p.line.path[3][3],p.line.path[3][4],.7);
-
-      p.line.path[1][1] = controlPoints[0];
-      p.line.path[1][2] = controlPoints[1];
+      var controlPoints = getControlPoints(p.line.path[1][5],p.line.path[1][6],p.line.path[2][5],p.line.path[2][6],p.line.path[3][5],p.line.path[3][6],.4);
+      // console.log(controlPoints);
+      p.line.path[2][3] = controlPoints[0];
+      p.line.path[2][4] = controlPoints[1];
 
       p.line.path[3][1] = controlPoints[2];
       p.line.path[3][2] = controlPoints[3];
+
+      p.line.path[3][3] = controlPoints[2];
+      p.line.path[3][4] = controlPoints[3];
+
+
+      controlPoints = getControlPoints(p.line.path[0][1],p.line.path[0][2],p.line.path[1][5],p.line.path[1][6],p.line.path[2][5],p.line.path[2][6],.4);
+      // console.log(controlPoints);
+      p.line.path[1][3] = controlPoints[0];
+      p.line.path[1][4] = controlPoints[1];
+
+      p.line.path[2][1] = controlPoints[2];
+      p.line.path[2][2] = controlPoints[3];
     }
   },
 });
