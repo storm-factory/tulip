6989734be395d83673d11bc5ed1515ed7a85c264
Drew Mitchell <drew.mitchell@wowza.com>
2015-10-26 23:18:41 -0600

    exchanged ol for google maps as I came up with a way to synthisize rotation without compromising UX and ol interactivity was above my head.

--- src/application.js
+++ src/application.js
@@ -5,14 +5,13 @@ var app = {};
 var wpt;
 var wpt1;
 var roadbook;
+function initMap() {
+  app.mapEditor = new MapEditor();
+}
 $(document).ready(function(){
-  mapEditor = new MapEditor();
   roadbook = new Roadbook();
   wpt = new Waypoint({totalDistance: 10, relativeDistance: 3, notes: 'here is some stuff'});
   wpt1 = new Waypoint({totalDistance: 11, relativeDistance: 1, notes: 'here is some more stuff'});
   roadbook.addWaypoint(wpt);
   roadbook.addWaypoint(wpt1);
-
-
-
 });
--- src/modules/mapping.js
+++ src/modules/mapping.js
@@ -1,72 +1,50 @@
-//36.811740, -108.286613
+
 var MapEditor = Class({
+  //maintain contecxt in listeners
+  _this: {},
+
   create: function(){
+    _this = this;
     this.initializeMap();
-    this.initializeHandlerControls();
+    this.initializeListeners();
+    this.routePath = poly.getPath();
   },
 
   initializeMap: function(){
-    this.waypoints = [];
-    this.vectorSource = new ol.source.Vector({
-      features: this.waypoints //add an array of features
-    });
-    this.vectorLayer = new ol.layer.Vector({
-                              source: this.vectorSource,
-                            });
-
-    this.map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.XYZ({
-              attributions: "ersi",
-              url: 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-            })
-          }),
-          this.vectorLayer
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([-108.286613, 36.811740]),
-          zoom: 10,
-          maxZoom: 19,
-          rotation: 0,
-        }),
-        interactions: ol.interaction.defaults({mouseWheelZoom:false}),
-      });
-  },
-
-  initializeHandlerControls: function(){
-    var _this = this;
-    this.map.on('singleclick', function(evt) {
-      _this.addWaypoint(evt.coordinate);
-     });
-
-  },
-
-  addWaypoint: function(espg3875LongLat){
-    var waypoint = new ol.Feature({
-      geometry: new ol.geom.Point(espg3875LongLat),
-      style: new ol.style.Style({
-        fill: new ol.style.Fill({
-          color: 'rgba(255, 255, 255, 0.2)'
-        }),
-        stroke: new ol.style.Stroke({
-          color: '#ffcc33',
-          width: 2
-        }),
-        image: new ol.style.Circle({
-          radius: 7,
-          fill: new ol.style.Fill({
-            color: '#ffcc33'
-          })
-        })
-      }),
+    this.map = new google.maps.Map(document.getElementById('map'), {
+       center: {lat: 36.068209, lng: -105.629669},
+       zoom: 4,
+       disableDefaultUI: true,
+       mapTypeId: google.maps.MapTypeId.HYBRID
     });
 
-    this.waypoints.push(waypoint)
-    console.log(espg3875LongLat);
-    console.log(this.vectorLayer);
-    this.vectorSource.set('features', this.waypoints);
+    this.attemptGeolocation();
 
+    poly = new google.maps.Polyline({
+      strokeColor: 'yellow',
+      strokeOpacity: 1.0,
+      strokeWeight: 3
+    });
+    poly.setMap(this.map);
+  },
+
+  initializeListeners: function() {
+    // Add a listener for the click event
+    this.map.addListener('click', this.addRoutePoint);
+  },
+
+  attemptGeolocation: function(){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      _this.map.setCenter(pos);
+      _this.map.setZoom(8);
+    });
+  },
+
+  addRoutePoint: function(evt){
+    _this.routePath.push(evt.latLng);
   },
 });
