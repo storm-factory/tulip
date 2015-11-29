3de6406a8982370cfe9e9c57fc838c745e4361f4
Drew Mitchell <jamesandrewmitchell@gmail.com>
2015-11-29 00:19:24 -0700

    more cleanup and styling changes

--- app/application.js
+++
@@ -1,8 +0,0 @@
-
-// var waypoints = new WaypointsCollection();
-
-// var wpt = new WaypointModel({totalDistance: 11, relativeDistance: 3})
-// var wpt1 = new WaypointModel({totalDistance: 12, relativeDistance: 1})
-//
-// var wptView = new WaypointView({model: wpt});
-// var wptView1 = new WaypointView({model: wpt1});
