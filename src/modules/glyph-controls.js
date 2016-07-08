b4c1b2a8fd9910939e616764de22eb7344c230e1
Drew Mitchell <drew.mitchell@wowza.com>
2016-07-07 23:20:06 -0600

    stubbing out file system

--- src/modules/glyph-controls.js
+++ src/modules/glyph-controls.js
@@ -2,6 +2,13 @@ var GlyphControls = Class({
   singleton: true,
 
   create: function(){
-    console.log('i am here!');
+    this.fs = require('fs');
+    this.getGylphNames();
+  },
+
+  getGylphNames(){
+    return this.fs.readdir('assets/svg/details', function(err, files){
+      console.log(files)
+    });
   },
 });
