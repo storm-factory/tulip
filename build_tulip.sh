70a4f2de9b60e05937397663298fe97cf1c474f8
Dustin Chilson <dustinchilson@users.noreply.github.com>
2021-12-26 19:11:35 +0000

    Add DevContainer

--- build_tulip.sh
+++ build_tulip.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-tap test/unit/*.js
+tape test/unit/*.js
 
 echo "what is the build version?"
 read VERSION
 echo "building tulip $VERSION for OSX"
-electron-packager . --appname=tulip --platform=darwin --arch=x64 --icon=tulip-logo.ico --app-version=$VERSION --version=1.2.4 --overwrite
+electron-packager . --appname=tulip --platform=darwin --arch=x64 --icon=tulip-logo.ico --app-version=$VERSION --overwrite
 echo "building tulip $VERSION for Windows"
-electron-packager . --appname=tulip --platform=win32 --arch=x64 --icon=tulip-logo.ico --app-version=$VERSION --version=1.2.4 --overwrite
+electron-packager . --appname=tulip --platform=win32 --arch=x64 --icon=tulip-logo.ico --app-version=$VERSION --overwrite
 
 echo "tulip builds complete!"
